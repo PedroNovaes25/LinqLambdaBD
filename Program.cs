0c0107a3a94a6177596c5da9c94a310761e012bc
PedroNovaes25 <dodroxpedro28@gmail.com>
2020-06-18 11:57:39 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introducao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+            foreach (var item in numeros.Where(n => n %2 == 0))
+            {
+                Console.Write(item + ", ");
+            }
+        }
+    }
+}
